04ef1bb115d4c42097621556046efcd147e1593d
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2025-01-09 20:54:02 -0500

    Add push notification handling and click event in service worker

--- public/sw.js
+++ public/sw.js
@@ -6,8 +6,28 @@ self.addEventListener("sync", (event) => {
     }
 });
 
+self.addEventListener("push", (event) => {
+    const options = {
+        body: event.data.text(),
+        icon: "https://cdn.pixabay.com/photo/2020/12/20/04/06/man-5846064_1280.jpg",
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1,
+        },
+        badge: "https://cdn.pixabay.com/photo/2020/12/20/04/06/man-5846064_1280.jpg",
+    };
+
+    event.waitUntil(self.registration.showNotification("Timeschedule", options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();
+});
+
+
 function mostrarNotificacion(titulo, mensaje) {
-   self.registration.showNotification(titulo, {
+    self.registration.showNotification(titulo, {
         body: mensaje,
         icon: "https://cdn.pixabay.com/photo/2020/12/20/04/06/man-5846064_1280.jpg",
     });
